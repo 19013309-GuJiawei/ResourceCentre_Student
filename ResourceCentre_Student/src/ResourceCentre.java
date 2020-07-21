2975552752b4af400395a310fe84a3fb22a258ad
19017010 <19017010@DESKTOP-SQ8KDJE>
2020-07-21 13:23:13 +0800

    LoanChromeBook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -282,6 +282,9 @@ public class ResourceCentre {
 	}
 	public static void loanChromebook(ArrayList<Chromebook> chromebookList) {
 		// write your code here
+		
+		//Jonathan
+		
 		ResourceCentre.viewAllChromebook(chromebookList);
 		String tag = Helper.readString("Enter asset tag > ");
 		String due = Helper.readString("Enter due date > ");
